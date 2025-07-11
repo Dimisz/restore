c7170adf7c5f84210720bd35c49f47562c1d20df
Vladimir Solovyov <leilyngale@icloud.com>
2025-07-11 11:38:23 +0800

    Scaffold client app

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+const products = [
+  {name: "product1", price: 100.00},
+  {name: "product2", price: 200.00},
+];
+
+function App() {
+
+  return (
+    <div>
+      <h1>Restore</h1>
+      <ul>
+        {products.map(item =>
+          <li key={item.name}>{item.name}: ${item.price}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
