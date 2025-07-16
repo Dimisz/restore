e0086bb6768008de58821e4060043c23d2798b66
Vladimir Solovyov <leilyngale@icloud.com>
2025-07-16 17:09:19 +0800

    Setup redux store and relace useEffect with RTK query

--- client/src/app/features/catalog/Catalog.tsx
+++ client/src/app/features/catalog/Catalog.tsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react";
-import type { Product } from "../../models/product";
+// import { useEffect, useState } from "react";
+// import type { Product } from "../../models/product";
 import ProductList from "./ProductList";
+import { useFetchProductsQuery } from "../contacts/catalogApi";
 
 
 export default function Catalog(){
-  const [products, setProducts] = useState<Product[]>([]);
-  useEffect(() => {
-      fetch('https://localhost:5001/api/products')
-        .then(response => response.json())
-        .then(data => setProducts(data))
-        .catch(e => console.log(`Error fetching data: ${e}`))
-    }, []);
+  // const [products, setProducts] = useState<Product[]>([]);
+  // useEffect(() => {
+  //     fetch('https://localhost:5001/api/products')
+  //       .then(response => response.json())
+  //       .then(data => setProducts(data))
+  //       .catch(e => console.log(`Error fetching data: ${e}`))
+  //   }, []);
+  // RELACE WITH THE RTK HOOK
+  const {data, isLoading} = useFetchProductsQuery();
+  if(isLoading || !data) return <div>Loading....</div>
+
   return(
-    <ProductList products={products} />
+    <ProductList products={data} />
   );
 }
